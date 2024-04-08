71e298fb8458f3984f7f57988c8b91bd8af42ea5
shallinan1 <hallisky@uw.edu>
2024-04-07 21:44:11 -0700

    Make directory if it doesn't exist

--- utils.py
+++ utils.py
@@ -1,11 +1,20 @@
 from unidecode import unidecode
 import json
+import os
 
 def clean_text(text):
     return unidecode(text)
 
 def save_to_jsonl(data, filename):
-    """Saves the data to a JSONL file."""
+    """Saves the data to a JSONL file, creating directories if they don't exist."""
+    # Extract the directory part from the filename
+    directory = os.path.dirname(filename)
+    
+    # If the directory part is not empty and does not exist, create it
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # Proceed to save the data to the file
     with open(filename, 'w', encoding='utf-8') as f:
         for item in data:
             json_record = json.dumps(item, ensure_ascii=False)
