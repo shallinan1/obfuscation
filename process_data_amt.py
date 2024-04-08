fecef198f2381f0dfceebfb9c6a033e62296bba7
shallinan1 <hallisky@uw.edu>
2024-04-08 11:29:36 -0700

    Fix AMT data

--- process_data_amt.py
+++ process_data_amt.py
@@ -23,26 +23,36 @@ for path, label in zip(paths, labels):
     with open(path, "rb") as f:
         data = pickle.load(f)
     print(f"Length of data is {len(data)} for {label}")
-    sents = []
+    final_data = {}
     for d in data:
+        cur_style = d[0].split("/")[1]
+        if cur_style not in final_data:
+            final_data[cur_style] = []
+
         d = clean_text(d[-1].strip())
         d = re.sub(r'\n\t', '\n', d)
         d = re.sub(r'\n+', '\n', d)
         paragraphs = d.split("\n")
         for p in paragraphs:
-            sents.append(sent_tokenize(p.strip()))
-            
+            final_data[cur_style].append(sent_tokenize(p.strip()))
+
     if label == "train":
         train_ratio = 0.75
         dev_ratio = 0.25
         random.seed(SEED)
-        train_sents, dev_sents = train_test_split(
-            sents, 
-            train_size=train_ratio, 
-            test_size=dev_ratio, 
-            shuffle=True, 
-            random_state=SEED)
-        save_to_jsonl(train_sents, f"data/train_para_amt-3.jsonl")
-        save_to_jsonl(dev_sents, f"data/val_para_amt-3.jsonl")
+
+        for cur_label in final_data.keys():
+            sents = final_data[cur_label]
+
+            train_sents, dev_sents = train_test_split(
+                sents, 
+                train_size=train_ratio, 
+                test_size=dev_ratio, 
+                shuffle=True, 
+                random_state=SEED)
+            save_to_jsonl(train_sents, f"data/train_para_amt-3-{cur_label}.jsonl")
+            save_to_jsonl(dev_sents, f"data/val_para_amt-3-{cur_label}.jsonl")
     else:
-        save_to_jsonl(sents, f"data/test_para_amt-3.jsonl")
+        for cur_label in final_data.keys():
+            sents = final_data[cur_label]
+            save_to_jsonl(sents, f"data/test_para_amt-3-{cur_label}.jsonl")
